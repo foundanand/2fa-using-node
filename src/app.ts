3b2a0525a17ec85b0d8479a98ba6787a1eca2d52
foundanand <anand.lahoti2021@vitbhopal.ac.in>
2024-07-29 19:10:55 +0530

    App and Index

--- src/app.ts
+++ src/app.ts
@@ -1,5 +1,13 @@
 import express from 'express';
+import bodyParser from 'body-parser';
+import { router as userRouter } from './routers/user.router';
 
 const app = express();
+require('dotenv').config();
+
+app.use(bodyParser.json());
+
+app.use('/api/v1/user', userRouter);
+
 
 export { app };
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,16 @@
 import { app } from './app';
+import connectDB from './config/db';
 
 const port = process.env.PORT || 3000;
+
 
 app.get('/', (req, res) => {
     res.send('2FA Server Running..');
   });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Server is running on port ${port}`);
+
+  // Connect to MongoDB
+  await connectDB();
 });
